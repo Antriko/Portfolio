5712657b54aae683359451681dc60a6e8ede4849
Antriko <creeperdrag772@gmail.com>
2020-03-10 11:22:50 +0000

    improvements

--- front/src/components/aboutMe.component.js
+++ front/src/components/aboutMe.component.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import description from './section/description.component.js';
-import projects from './section/projects.component.js';
 
 export default class aboutMe extends Component {
     render() {
         return (
             <React.Fragment>
                 {description()}
-                {projects()}
             </React.Fragment>
         );
     };
--- front/src/components/section/description.component.js
+++ front/src/components/section/description.component.js
@@ -8,15 +8,17 @@ export default function description() {
                     <div className="row">
                         <h4>Self-taught Software Developer</h4>
                         <p>
-                            What first brought me into coding was the Computer Science subject in Secondary School, I was naturally able to quickly grasp the practical side of coding more than the theory and found fun in problem solving through logical thinking.<br/><br/>
+                            What first brought me into coding was the Computer Science subject in Secondary School, I was naturally able to quickly grasp the practical side of coding more than the theory and found fun in problem-solving through logical thinking.<br/><br/>
 
-                            Ever since then, I have delved deeper into Computer Science by continuing it into college however was unable to complete the course due to personal issues. 
-                            I still wanted to continue developing my coding skills so I looked into any possibilities that could help me do so, finding an apprenticeship which helped newcomers specifically into JavaScript.<br/><br/>
+                            Ever since then, I have delved deeper into Computer Science by continuing it into college where I got a better thinking of the theoretical side of computing as well as learning Python. 
+                            After college, I still wanted to continue developing my coding skills so I looked into any possibilities that could help me do so, eventually landing on an apprenticeship that helped newcomers specifically into JavaScript.<br/><br/>
 
-                            I have started off coding at a more professional level when I was introduced to a game making apprenticeship, successfully creating multiple small games that was 
+                            I have started off coding at a more professional level when I was introduced to a game making apprenticeship, successfully creating multiple small games that were
                             playable on the phone and then moved on to creating websites as I have found more interest in creating web applications than games and this was a good starting ground.<br/><br/>
                             
-                            At home, I occasionally create small projects to learn new technology which was not shown to me such as database management and creating a full-stack website with some form of database implementation.
+                            At home, I have used those skills that I have learned at work to help me research for a better understanding of software development, learning about frameworks that can build fully functioning user interfaces, such as learning the MERN stack.<br/><br/>
+
+                            I have previously worked in a scrum environment which has allowed me to easily work with other developers to create a finished product in time for the client to be happy.
                         </p>
                     </div>
                 </div>
