807ece8e73eceaf13952aee7fc9731d9c0b4e79c
Antriko <creeperdrag772@gmail.com>
2023-09-02 07:14:41 +0100

    init

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ touch logs/backend.txt
 docker-compose -f docker-compose.initial.yml build
 docker-compose -f docker-compose.initial.yml up -d nginx
 echo "🔍 Hit ctrl+d after you are done with certbot!"
-docker-compose exec nginx certbot certonly --nginx -d cloud.antriko.co.uk
+docker-compose exec nginx certbot certonly --nginx -d www.antriko.co.uk -d antriko.co.uk
 docker-compose down
 echo "🚀 Done!"
