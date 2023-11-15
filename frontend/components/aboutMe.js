f9a2c5940e1d13464b96c0f0817d7681f1165ae1
Antriko <creeperdrag772@gmail.com>
2023-11-15 14:36:19 +0000

    text

--- frontend/components/aboutMe.js
+++ frontend/components/aboutMe.js
@@ -2,16 +2,16 @@ const AboutMe = () => {
     return(
         <div className="w-full bg-light p-4 flex-row justify-center py-24 text-black px-8 sm:px-24" id="about">
             <div className="pb-4">
-                What first brought me into coding was the Computer Science subject in Secondary School. I quickly discovered that I had a natural knack for applying code practically, often finding more joy in solving problems through logical thinking than delving into the theory behind it.
+                What first brought me into coding was the Computer Science subject in Secondary School. I quickly discovered that I had a natural knack for applying code practically, often finding joy in solving problems through logical thinking and problem solving.
             </div>
             <div className="pb-4">
                 As I progressed through my education, I continued to explore the subject of Computer Science in college, where I gained a deeper understanding of the theoretical side of computing to improve on my skills.
             </div>
             <div className="pb-4">
-                Since then, I enrolled in a Software Engineering apprenticeship program for over 2 years. Throughout the apprenticeship journey, I was able to recieve hands-on expereience in both mobile game development and web development.
+                Since then, I enrolled in a Software Engineering apprenticeship program for over 2 years. Throughout the apprenticeship journey, I was able to recieve hands-on expereience in both mobile game development and web development.    
             </div>
             <div className="pb-4">
-                After my apprenticeship, I went to get further education for Software Engineering by going for a Bachelors degree for Software Engineering in Regent College London. With dedication and hard work, I not only completed the program but achieved the distinction of graduating with first-class honors.
+                After my apprenticeship, I went to get further education by going for a Bachelors of Engineering degree for Software Engineering awarded by University of Bolton. With dedication and hard work, I not only completed the program but achieved the distinction of graduating with first-class honors.
             </div>
         </div>
     );
